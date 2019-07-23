afc2109b8f79ce0bbfab349c69157a3d9151e059
Javier Sanchez <javsan77@gmail.com>
2019-07-23 14:42:48 -0500

    Create code.php

---
+++ code.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es-es">
+	<head>
+        <!-- RESP -->
+        <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=yes">
+        <!-- RESP -->
+        
+        <!-- More code not important for this explanation... -->
+        
+	<style>
+  
+  		/** RESP **/
+		@media only screen and (max-width: 700px) {
+
+		    * {
+		        font-size: 13px ;
+		    }
+			/** Hiding title  */
+			.dhx_toolbar_text{
+				display: none;
+			}
+
+			/** Hiding title of text of action icons **/
+			html body#afr-body.dhxlayout_base_dhx_web.dhxwins_vp_dhx_web div.dhxlayout_cont div.dhx_cell_layout div.dhx_cell_toolbar_def div.dhx_toolbar_dhx_web.dhxtoolbar_icons_18.dhx_toolbar_shadow div.dhxtoolbar_float_left div.dhx_toolbar_btn.dhxtoolbar_btn_def div.dhxtoolbar_text{
+				display: none;
+			}
+
+			/** Accomodating to the right to the arrow ...  */
+			.dhxlayout_arrow.dhxlayout_arrow_va {
+				margin-right: 0px !important;
+			}
+
+			/** Expanding zone left to make click  */
+			.dhxlayout_base_dhx_web div.dhx_cell_layout div.dhx_cell_hdr div.dhxlayout_arrow {
+				right: 0px;
+				width: 20px;
+			    height: 100%;
+			}
+
+			.dhxlayout_cont {
+
+				left: 5px !important;
+
+			}
+
+		  #logo_superior{
+				display: none;
+			}
+
+		}
+		/** RESP **/
+  
+  </style>
+</head>  
